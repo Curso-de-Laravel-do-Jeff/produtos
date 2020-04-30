925b2a1b67ed28e184f924dcc271f89b1e6f5326
Jefferson Frade <jefferrson.frade@gmail.com>
2020-04-30 20:11:50 -0300

    First commit

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -13,7 +13,7 @@
             <tr>
                 <th class="text-center">ID</th>
                 <th class="text-center">Name</th>
-                <th class="text-center">Price</th>
+                <th class="text-center">Price (U$)</th>
                 <th class="text-center">Category</th>
             </tr>
         </thead>
@@ -23,7 +23,7 @@
                 <tr>
                     <td class="text-center">{{ $product->id }}</td>
                     <td class="text-center">{{ $product->name }}</td>
-                    <td class="text-center">{{ $product->price }}</td>
+                    <td class="text-center">{{ StringHelper::formatMoney($product->price) }}</td>
                     <td class="text-center">{{ $product->category->category }}</td>
                 </tr>
             @empty
